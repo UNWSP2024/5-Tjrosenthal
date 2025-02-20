991e5e3c5e5633fd04c82329c2fc2755d54924e7
Tjrosenthal <tjrosenthal@students.unwsp.edu>
2025-02-20 09:21:41 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,25 @@
 # Program #2: Math Quiz
 # Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
 
-#     247
+#Tanner Rosenthal
+#2/20/2025
 
-# + 129
+import random
 
-# ------
+def quiz():
+    random1 = random.randint(100, 999)
+    random2 = random.randint(100, 999)
+    print("     ", random1)
+    print("+    ", random2)
+    print("---------")
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
-# The program must use a function that accomplishes part of the needed tasks.
+    user_answer = int(input("what is the answer?"))
+    correct_answer = random1 + random2
+
+    if user_answer == correct_answer:
+        print("That is correct!")
+
+    else:
+        print("That is incorrect. The correct answer is", correct_answer)
+
+quiz()
